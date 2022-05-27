c2218be97bd204320334e764947aaceb7a5cc00a
Jake Freck <jacobfreck@gmail.com>
2022-05-27 16:54:27 -0700

    Initial

---
+++ CircularBuffer.Tests/CircularBufferTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CircularBuffer.Tests;
+
+public class CircularBufferTests
+{
+    ITestOutputHelper Logger { get; init; }
+
+    public CircularBufferTests(ITestOutputHelper testOutputHelper)
+    {
+        Logger = testOutputHelper;
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(int.MinValue)]
+    public void InvalidSizeBuffer_ThrowsArgumentException(int size)
+    {
+        Assert.Throws<ArgumentException>(() => new CircularBuffer<int>(size));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(3)]
+    [InlineData(5)]
+    public void AddItem_Success(int size)
+    {
+        CircularBuffer<int> buffer = new(size);
+        buffer.Add(0);
+        Assert.Single(buffer);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(3)]
+    [InlineData(5)]
+    public void RemoveItem_Success(int size)
+    {
+        CircularBuffer<int> buffer = new(size);
+
+        buffer.Add(0);
+        buffer.Remove();
+
+        foreach (int item in buffer)
+            Console.WriteLine($"item {item}");
+
+        Assert.Empty(buffer);
+    }
+
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(3)]
+    [InlineData(5)]
+    public void AddFull_Enumerate(int size)
+    {
+        CircularBuffer<int> buffer = new(size);
+
+        foreach (int i in Enumerable.Range(0, size))
+            buffer.Add(i);
+
+        int count = 0;
+        foreach (int item in buffer)
+        {
+            Assert.Equal(count, item);
+            count++;
+        }
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(6)]
+    public void AddOverflow(int size)
+    {
+        CircularBuffer<int> buffer = new(size);
+
+        foreach (int i in Enumerable.Range(0, size+1))
+            buffer.Add(i);
+
+        foreach ((int value, int index) in buffer.Select((int i, int index) => (i, index)))
+        {
+            if (index == 0)
+                Assert.Equal(size, value);
+            else
+                Assert.Equal(index, value);
+
+            Logger.WriteLine($"value={value}, index={index}");
+        }
+
+        // TODO: assert values
+        Assert.Equal(size, buffer.Count);
+    }
+}
---
+++ CircularBuffer/CircularBuffer.cs
@@ -0,0 +1,91 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CircularBuffer.Tests")]
+
+namespace System.Collections.Generic;
+
+public class CircularBuffer<T> : IEnumerable, IEnumerable<T>
+{
+    internal T[] _arr;
+    internal int _start;
+    internal int _size;
+
+    public CircularBuffer(int size)
+    {
+        if (size <= 0)
+            throw new ArgumentException($"Invalid argument {nameof(size)} with value {size}");
+
+        _arr = new T[size];
+        _start = 0;
+        _size = 0;
+    }
+
+    public int Count => _size;
+
+    public void Add(T item)
+    {
+        int nextIndex = (_start + _size) % _arr.Length;
+        _size = Math.Min(_size + 1, _arr.Length);
+        _arr[nextIndex] = item;
+    }
+
+    public void Clear()
+    {
+        Array.Clear(_arr);
+        _start = 0;
+        _size = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        for (int i = _start; i < _start + _size; i++)
+        {
+            int nextIndex = (_start + _size) % _arr.Length;
+            T other = _arr[nextIndex];
+            if (other is not null && other.Equals(item))
+                return true;
+        }
+
+        return false;
+    }
+
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        if ((array != null) && (array.Rank != 1))
+        {
+            throw new ArgumentException("Multi-dimensional arrays not supported");
+        }
+
+        try
+        {
+            // Array.Copy will check for NULL.
+            Array.Copy(_arr, 0, array, arrayIndex, _size);
+        }
+        catch (ArrayTypeMismatchException ex)
+        {
+            throw new ArgumentException("Array type mismatch {exception}", ex);
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = _start; i < _start + _size; i++)
+            yield return _arr[i % _arr.Length];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public bool Remove()
+    {
+        if (_size == 0)
+            return false;
+
+        _size--;
+
+        return true;
+    }
+}
