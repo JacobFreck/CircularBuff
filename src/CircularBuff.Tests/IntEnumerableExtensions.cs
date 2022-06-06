bec625d751702073aff1d7302144af65ebe51a77
Jake Freck <JacobFreck@users.noreply.github.com>
2022-06-05 19:40:00 -0700

    Fix Start Index (#8)
    * fix start index
    
    * fix

---
+++ src/CircularBuff.Tests/IntEnumerableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CircularBuff.Tests;
+
+public static class IntEnumerableExtensions
+{
+
+    public static IEnumerable<int> To(this int from, int to)
+    {
+        if (from < to)
+        {
+            while (from <= to)
+            {
+                yield return from++;
+            }
+        }
+        else
+        {
+            while (from >= to)
+            {
+                yield return from--;
+            }
+        }
+    }
+
+    public static IEnumerable<T> Step<T>(this IEnumerable<T> source, int step)
+    {
+        if (step == 0)
+        {
+            throw new ArgumentOutOfRangeException("step", "Param cannot be zero.");
+        }
+
+        return source.Where((x, i) => (i % step) == 0);
+    }
+}
+
